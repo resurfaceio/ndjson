73650c995c40e23e33256d17cb74b1ab2c6f80bf
Rob Dickinson <robfromboulder@gmail.com>
2020-12-12 12:57:14 -0700

    Remap exception handling

--- src/main/java/io/resurface/messages/Messages.java
+++ src/main/java/io/resurface/messages/Messages.java
@@ -13,31 +13,36 @@ public class Messages {
     /**
      * Returns reader to ndjson file.
      */
-    public static BufferedReader open(String file) throws IOException {
-        if (file.endsWith(".ndjson")) {
-            return new BufferedReader(new FileReader(file));
-        } else if (file.endsWith(".ndjson.gz")) {
-            return new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(file))));
-        } else {
-            throw new IllegalArgumentException("File is not .ndjson or .ndjson.gz format");
+    public static BufferedReader open(String file) {
+        try {
+            if (file.endsWith(".ndjson")) {
+                return new BufferedReader(new FileReader(file));
+            } else if (file.endsWith(".ndjson.gz")) {
+                return new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(file))));
+            } else {
+                throw new IllegalArgumentException("File is not .ndjson or .ndjson.gz format");
+            }
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
         }
     }
 
     /**
      * Process all lines in ndjson file.
      */
-    public static void process(String file, LineProcessor processor) throws IOException {
+    public static void process(String file, LineProcessor processor) {
         try (BufferedReader reader = open(file)) {
             String line;
             while ((line = reader.readLine()) != null) processor.process(line);
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
         }
-
     }
 
     /**
      * Process all messages in ndjson file.
      */
-    public static void process(String file, MessageProcessor processor) throws IOException {
+    public static void process(String file, MessageProcessor processor) {
         process(file, (LineProcessor) line -> processor.process(new HttpMessage(line)));
     }
 
