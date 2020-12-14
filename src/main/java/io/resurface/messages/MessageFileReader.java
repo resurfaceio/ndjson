1dc36157cef20c396dd00a4aa5a5b23ff54b7210
Rob Dickinson <robfromboulder@gmail.com>
2020-12-14 00:42:27 -0700

    Add file helpers

---
+++ src/main/java/io/resurface/messages/MessageFileReader.java
@@ -0,0 +1,59 @@
+// © 2016-2020 Resurface Labs Inc.
+
+package io.resurface.messages;
+
+import java.io.*;
+import java.util.zip.GZIPInputStream;
+
+/**
+ * Reads from compressed NDJSON file.
+ */
+public class MessageFileReader implements AutoCloseable, Closeable {
+
+    /**
+     * Opens file for reading.
+     */
+    public MessageFileReader(String file) {
+        if (!file.endsWith(".ndjson.gz"))
+            throw new IllegalArgumentException("File is not .ndjson.gz format");
+
+        // todo throw exception if file doesn't already exist?
+
+        this.file = file;
+    }
+
+    /**
+     * Closes file.
+     */
+    public void close() {
+        // do nothing
+    }
+
+    /**
+     * Iterates all lines in file without parsing first.
+     */
+    public void iterate(LineProcessor processor) {
+        try (FileInputStream fos = new FileInputStream(file)) {
+            try (GZIPInputStream gos = new GZIPInputStream(fos)) {
+                try (InputStreamReader isr = new InputStreamReader(gos)) {
+                    try (BufferedReader br = new BufferedReader(isr)) {
+                        String line;
+                        while ((line = br.readLine()) != null) processor.process(line);
+                    }
+                }
+            }
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+
+    /**
+     * Parses and processes all messages in file.
+     */
+    public void parse(MessageProcessor processor) {
+        iterate(line -> processor.process(new HttpMessage(line)));
+    }
+
+    private final String file;
+
+}
---
+++ src/main/java/io/resurface/messages/MessageFileWriter.java
@@ -0,0 +1,67 @@
+// © 2016-2020 Resurface Labs Inc.
+
+package io.resurface.messages;
+
+import java.io.*;
+import java.util.zip.GZIPOutputStream;
+
+/**
+ * Writes to compressed NDJSON file.
+ */
+public class MessageFileWriter implements AutoCloseable, Closeable, Flushable {
+
+    /**
+     * Opens file for writing.
+     */
+    public MessageFileWriter(String file) {
+        if (!file.endsWith(".ndjson.gz"))
+            throw new IllegalArgumentException("File is not .ndjson.gz format");
+
+        // todo throw exception if file already exists?
+
+        try {
+            fos = new FileOutputStream(file);
+            writer = new BufferedWriter(new OutputStreamWriter(new GZIPOutputStream(fos)));
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+
+    /**
+     * Closes file.
+     */
+    public void close() {
+        try {
+            fos.close();
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+
+    /**
+     * Flushes any buffered writes.
+     */
+    public void flush() {
+        try {
+            writer.flush();
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+
+    /**
+     * Writes message to file.
+     */
+    public void write(HttpMessage message) {
+        try {
+            writer.write(message.toString());
+            writer.newLine();
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+
+    private final FileOutputStream fos;
+    private final BufferedWriter writer;
+
+}
